15ee181d21a055b49c784f750b0af376b41e4240
Maxi <taki-s4@hotmail.com>
2024-07-29 19:30:51 -0300

    commit5

--- tienda_videojuegos/tienda/admin.py
+++ tienda_videojuegos/tienda/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tienda.models import Videojuego, Consola, Accesorio
 
 # Register your models here.
+
+admin.site.register(Videojuego)
+admin.site.register(Consola)
+admin.site.register(Accesorio)
